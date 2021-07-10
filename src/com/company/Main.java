cc03461e0f95f08be9817d71d8c77c9341321211
Sascha Freudiger <s.freudiger@bluewin.ch>
2021-07-10 18:16:28 +0200

    Created test() Method
    Created test() Method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+    test();
+    }
+
+    private static void test() {
+
+        System.out.println("Hello World");
     }
 }
