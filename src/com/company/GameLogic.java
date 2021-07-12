500af772746b9178de3f8972e0471156caf5f501
Sascha Freudiger <s.freudiger@bluewin.ch>
2021-07-12 22:05:01 +0200

    Correct IsFieldOccupied
    Correct IsFieldOccupied

--- src/com/company/GameLogic.java
+++ src/com/company/GameLogic.java
@@ -6,186 +6,190 @@ package com.company;
  * own in a row wins the game.
  */
 public class GameLogic {
+    private String field1 = "";
+    private String field2 = "";
+    private String field3 = "";
+    private String field4 = "";
+    private String field5 = "";
+    private String field6 = "";
+    private String field7 = "";
+    private String field8 = "";
+    private String field9 = "";
 
-	private String field1 = "";
-	private String field2 = "";
-	private String field3 = "";
-	private String field4 = "";
-	private String field5 = "";
-	private String field6 = "";
-	private String field7 = "";
-	private String field8 = "";
-	private String field9 = "";
+    private int input = 0;
 
-	private int input = 0;
+    Player player1 = new Player("Sascha");
+    Player player2 = new Player("Steven");
+    public void makeTurn() {
 
-	Player player1 = new Player("Sascha");
-	Player player2 = new Player("Steven");
+        input = Main.playerInput();
+        fieldIsOccupied(input);
+        if (input == 1 && field1.equals("")) {
+            field1 = setXorO();
+            setCurrentPlayer();
+        }
+        if (input == 2 && field2.equals("")) {
+            field2 = setXorO();
+            setCurrentPlayer();
+        }
+        if (input == 3 && field3.equals("")) {
+            field3 = setXorO();
+            setCurrentPlayer();
+        }
+        if (input == 4 && field4.equals("")) {
+            field4 = setXorO();
+            setCurrentPlayer();
+        }
+        if (input == 5 && field5.equals("")) {
+            field5 = setXorO();
+            setCurrentPlayer();
+        }
+        if (input == 6 && field6.equals("")) {
+            field6 = setXorO();
+            setCurrentPlayer();
+        }
+        if (input == 7 && field7.equals("")) {
+            field7 = setXorO();
+            setCurrentPlayer();
+        }
+        if (input == 8 && field8.equals("")) {
+            field8 = setXorO();
+            setCurrentPlayer();
+        }
+        if (input == 9 && field9.equals("")) {
+            field9 = setXorO();
+            setCurrentPlayer();
+        }
+        if (IsThereWinner())
+            System.out.println("Gewinner ist: " + WhoIsWinner());
+    }
 
-	/**
-	 * Method sets the current active Player.
-	 * 
-	 * @return returns active Player which has to make his turn.
-	 */
-	public void setCurrentPlayer() {
-//ToDo: Add If Else to evaluate which was the last player and then set active player.
-		if (player1.isOnTurn() == false && player2.isOnTurn() == false) {
-			player1.setOnTurn(true);
-			player2.setOnTurn(false);
-		}
-		if (player1.isOnTurn() == false && player2.isOnTurn() == true) {
-			player1.setOnTurn(true);
-			player2.setOnTurn(false);
-		} else {
-			player1.setOnTurn(false);
-			player2.setOnTurn(true);
-		}
+    /**
+     * Method sets the current active Player.
+     *
+     * @return returns active Player which has to make his turn.
+     */
+    public void setCurrentPlayer() {
 
-	}
+        if (player1.isOnTurn() == false && player2.isOnTurn() == false) {
+            player1.setOnTurn(true);
+            player2.setOnTurn(false);
+        }
+        if (player1.isOnTurn() == false && player2.isOnTurn() == true) {
+            player1.setOnTurn(true);
+            player2.setOnTurn(false);
+        } else {
+            player1.setOnTurn(false);
+            player2.setOnTurn(true);
+        }
 
-	public Player whoIsCurrentPlayer() {
-		Player currentPlayer = player1;
-		if (player1.isOnTurn() == true)
-			currentPlayer = player1;
-		if (player2.isOnTurn() == true)
-			currentPlayer = player2;
-		return currentPlayer;
-	}
+    }
 
-	/**
-	 *
-	 * @return returns X or O. It depends on the Symbol of the active Player
-	 */
-	public String setXorO() {
-		String symbol = null;
-		if (whoIsCurrentPlayer() == player1)
-			symbol = "X";
-		if (whoIsCurrentPlayer() == player2)
-			symbol = "O";
-		return symbol;
-	}
+    public Player whoIsCurrentPlayer() {
+        Player currentPlayer = player1;
+        if (player1.isOnTurn() == true)
+            currentPlayer = player1;
+        if (player2.isOnTurn() == true)
+            currentPlayer = player2;
+        return currentPlayer;
+    }
 
-	public boolean fieldIsOccupied() {
-		boolean b = false;
-		if (input == 1 && !field1.equals("")) {
-			System.out.println("Feld ist besetzt. Anderes Feld wählen.");
-			b = true;
-		}
-		if (input == 2 && !field2.equals("")) {
-			System.out.println("Feld ist besetzt. Anderes Feld wählen.");
-			b = true;
-		}
-		if (input == 3 && !field3.equals("")) {
-			System.out.println("Feld ist besetzt. Anderes Feld wählen.");
-			b = true;
-		}
-		if (input == 4 && !field4.equals("")) {
-			System.out.println("Feld ist besetzt. Anderes Feld wählen.");
-			b = true;
-		}
-		if (input == 5 && !field5.equals("")) {
-			System.out.println("Feld ist besetzt. Anderes Feld wählen.");
-			b = true;
-		}
-		if (input == 6 && !field6.equals("")) {
-			System.out.println("Feld ist besetzt. Anderes Feld wählen.");
-			b = true;
-		}
-		if (input == 7 && !field7.equals("")) {
-			System.out.println("Feld ist besetzt. Anderes Feld wählen.");
-			b = true;
-		}
-		if (input == 8 && !field8.equals("")) {
-			System.out.println("Feld ist besetzt. Anderes Feld wählen.");
-			b = true;
-		}
-		if (input == 9 && !field9.equals("")) {
-			System.out.println("Feld ist besetzt. Anderes Feld wählen.");
-			b = true;
-		}
-		return b;
-	}
+    /**
+     * @return returns X or O. It depends on the Symbol of the active Player
+     */
+    public String setXorO() {
+        String symbol = null;
+        if (whoIsCurrentPlayer() == player1)
+            symbol = "X";
+        if (whoIsCurrentPlayer() == player2)
+            symbol = "O";
+        return symbol;
+    }
 
-	public void makeTurn() {
+    public boolean fieldIsOccupied(int input) {
+        boolean b = false;
+        if (input == 1 && !field1.equals("")) {
+            System.out.println("Feld ist besetzt. Anderes Feld wählen.");
+            b = true;
+        }
+        if (input == 2 && !field2.equals("")) {
+            System.out.println("Feld ist besetzt. Anderes Feld wählen.");
+            b = true;
+        }
+        if (input == 3 && !field3.equals("")) {
+            System.out.println("Feld ist besetzt. Anderes Feld wählen.");
+            b = true;
+        }
+        if (input == 4 && !field4.equals("")) {
+            System.out.println("Feld ist besetzt. Anderes Feld wählen.");
+            b = true;
+        }
+        if (input == 5 && !field5.equals("")) {
+            System.out.println("Feld ist besetzt. Anderes Feld wählen.");
+            b = true;
+        }
+        if (input == 6 && !field6.equals("")) {
+            System.out.println("Feld ist besetzt. Anderes Feld wählen.");
+            b = true;
+        }
+        if (input == 7 && !field7.equals("")) {
+            System.out.println("Feld ist besetzt. Anderes Feld wählen.");
+            b = true;
+        }
+        if (input == 8 && !field8.equals("")) {
+            System.out.println("Feld ist besetzt. Anderes Feld wählen.");
+            b = true;
+        }
+        if (input == 9 && !field9.equals("")) {
+            System.out.println("Feld ist besetzt. Anderes Feld wählen.");
+            b = true;
+        }
+        return b;
+    }
 
-		input = Main.playerInput();
 
-		if (input == 1 && field1.equals("")) {
-			field1 = setXorO();
-		}
-		if (input == 2 && field2.equals("")) {
-			field2 = setXorO();
-		}
-		if (input == 3 && field3.equals("")) {
-			field3 = setXorO();
-		}
-		if (input == 4 && field4.equals("")) {
-			field4 = setXorO();
-		}
-		if (input == 5 && field5.equals("")) {
-			field5 = setXorO();
-		}
-		if (input == 6 && field6.equals("")) {
-			field6 = setXorO();
-		}
-		if (input == 7 && field7.equals("")) {
-			field7 = setXorO();
-		}
-		if (input == 8 && field8.equals("")) {
-			field8 = setXorO();
-		}
-		if (input == 9 && field9.equals("")) {
-			field9 = setXorO();
-		}
 
-		if (IsThereWinner())
-			System.out.println("Gewinner ist: " + WhoIsWinner());
-		
+    public boolean IsThereWinner() {
+        boolean winner = false;
+        if ((field1.equals("X") && field2.equals("X") && field3.equals("X"))
+                || (field4.equals("X") && field5.equals("X") && field6.equals("X"))
+                || (field7.equals("X") && field8.equals("X") && field9.equals("X"))
+                || (field1.equals("X") && field4.equals("X") && field7.equals("X"))
+                || (field2.equals("X") && field5.equals("X") && field8.equals("X"))
+                || (field3.equals("X") && field6.equals("X") && field9.equals("X"))
+                || (field1.equals("X") && field5.equals("X") && field9.equals("X"))
+                || (field3.equals("X") && field5.equals("X") && field7.equals("X"))) {
+            winner = true;
+            player1.setHasWon(true);
+        }
+        if ((field1.equals("O") && field2.equals("O") && field3.equals("O"))
+                || (field4.equals("O") && field5.equals("O") && field6.equals("O"))
+                || (field7.equals("O") && field8.equals("O") && field9.equals("O"))
+                || (field1.equals("O") && field4.equals("O") && field7.equals("O"))
+                || (field2.equals("O") && field5.equals("O") && field8.equals("O"))
+                || (field3.equals("O") && field6.equals("O") && field9.equals("O"))
+                || (field1.equals("O") && field5.equals("O") && field9.equals("O"))
+                || (field3.equals("O") && field5.equals("O") && field7.equals("O"))) {
+            winner = true;
+            player2.setHasWon(true);
+        }
+        return winner;
+    }
 
-	}
+    public String WhoIsWinner() {
+        Player winner = null;
+        if (player1.isHasWon())
+            winner = player1;
+        if (player2.isHasWon())
+            winner = player2;
+        return winner.getName();
+    }
 
-	public boolean IsThereWinner() {
-		boolean winner = false;
-		if ((field1.equals("X") && field2.equals("X") && field3.equals("X"))
-				|| (field4.equals("X") && field5.equals("X") && field6.equals("X"))
-				|| (field7.equals("X") && field8.equals("X") && field9.equals("X"))
-				|| (field1.equals("X") && field4.equals("X") && field7.equals("X"))
-				|| (field2.equals("X") && field5.equals("X") && field8.equals("X"))
-				|| (field3.equals("X") && field6.equals("X") && field9.equals("X"))
-				|| (field1.equals("X") && field5.equals("X") && field9.equals("X"))
-				|| (field3.equals("X") && field5.equals("X") && field7.equals("X"))) {
-			winner = true;
-			player1.setHasWon(true);
-		}
-		if ((field1.equals("O") && field2.equals("O") && field3.equals("O"))
-				|| (field4.equals("O") && field5.equals("O") && field6.equals("O"))
-				|| (field7.equals("O") && field8.equals("O") && field9.equals("O"))
-				|| (field1.equals("O") && field4.equals("O") && field7.equals("O"))
-				|| (field2.equals("O") && field5.equals("O") && field8.equals("O"))
-				|| (field3.equals("O") && field6.equals("O") && field9.equals("O"))
-				|| (field1.equals("O") && field5.equals("O") && field9.equals("O"))
-				|| (field3.equals("O") && field5.equals("O") && field7.equals("O"))) {
-			winner = true;
-			player2.setHasWon(true);
-		}
-		return winner;
-	}
-
-	public String WhoIsWinner() {
-		Player winner = null;
-		if (player1.isHasWon())
-			winner = player1;
-		if (player2.isHasWon())
-			winner = player2;
-		return winner.getName();
-	}
-
-	public void TestOutput() {
-		System.out.println("Feld 1: " + field1 + " Feld 2: " + field2 + " Feld 3: " + field3);
-		System.out.println("Feld 4: " + field4 + " Feld 5: " + field5 + " Feld 6: " + field6);
-		System.out.println("Feld 7: " + field7 + " Feld 8: " + field8 + " Feld 9: " + field9);
-
-		System.out.println(whoIsCurrentPlayer().getName());
-	}
+    public void TestOutput() {
+        System.out.println("Feld 1: " + field1 + " Feld 2: " + field2 + " Feld 3: " + field3);
+        System.out.println("Feld 4: " + field4 + " Feld 5: " + field5 + " Feld 6: " + field6);
+        System.out.println("Feld 7: " + field7 + " Feld 8: " + field8 + " Feld 9: " + field9);
+        System.out.println(whoIsCurrentPlayer().getName());
+    }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,37 +3,28 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-	private int input;
-	private static GameLogic logic;
+    private int input;
+    private static GameLogic logic;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        logic = new GameLogic();
+        int i = 0;
+        while (!logic.IsThereWinner()) {
+            logic.makeTurn();
+            logic.TestOutput();
 
-		logic = new GameLogic();
+            i++;
+        }
+        System.out.println("Das Spiel ist fertig. " + logic.WhoIsWinner() + " hat gewonnen.");
+    }
 
-		int i = 0;
-		// ToDo: replace condition of while loop
-		while (!logic.IsThereWinner()) {
+    public static int playerInput() {
 
-			
-				logic.makeTurn();
-				
-				logic.TestOutput();
-				logic.setCurrentPlayer();
-				i++;
-			
-		}
+        Scanner scan = new Scanner(System.in);
+        String currentPlayerName = logic.whoIsCurrentPlayer().getName();
+        System.out.println("Spieler " + currentPlayerName + " wähle ein Feld:");
+        return scan.nextInt();
 
-		System.out.println("Das Spiel ist fertig.");
-
-	}
-
-	public static int playerInput() {
-
-		Scanner scan = new Scanner(System.in);
-		String currentPlayerName = logic.whoIsCurrentPlayer().getName();
-		System.out.println("Spieler " + currentPlayerName + " wähle ein Feld:");
-		return scan.nextInt();
-
-	}
+    }
 
 }
